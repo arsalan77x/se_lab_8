23bbec4a40a8c5b95e6c45829751632130bb40e0
faraz1380 <Faraz.ghahremani@yahoo.com>
2024-05-31 12:35:54 +0330

    5-SeperateQueryFromModifier

--- MiniJava/src/main/java/codeGenerator/Memory.java
+++ MiniJava/src/main/java/codeGenerator/Memory.java
@@ -9,50 +9,43 @@ public class Memory {
     private ArrayList<_3AddressCode> codeBlock;
     private int lastTempIndex;
     private int lastDataAddress;
-    private final int stratTempMemoryAddress = 500;
-    private final int stratDataMemoryAddress = 200;
+    private final int startTempMemoryAddress = 500;
+    private final int startDataMemoryAddress = 200;
     private final int dataSize = 4;
     private final int tempSize = 4;
 
     public Memory() {
         codeBlock = new ArrayList<_3AddressCode>();
-        lastTempIndex = stratTempMemoryAddress;
-        lastDataAddress = stratDataMemoryAddress;
+        lastTempIndex = startTempMemoryAddress;
+        lastDataAddress = startDataMemoryAddress;
     }
 
-    public int getTemp() {
+    // Modifier methods
+    public void incrementTemp() {
         lastTempIndex += tempSize;
-        return lastTempIndex - tempSize;
     }
 
-    public int getDateAddress() {
+    public void incrementDataAddress() {
         lastDataAddress += dataSize;
-        return lastDataAddress - dataSize;
     }
 
-    public int saveMemory() {
-        codeBlock.add(new _3AddressCode());
-        return codeBlock.size() - 1;
+    // Query methods
+    public int getCurrentTemp() {
+        return lastTempIndex;
+    }
+
+    public int getCurrentDataAddress() {
+        return lastDataAddress;
     }
 
     public void add3AddressCode(Operation op, Address opr1, Address opr2, Address opr3) {
         codeBlock.add(new _3AddressCode(op, opr1, opr2, opr3));
     }
 
-    public void add3AddressCode(int i, Operation op, Address opr1, Address opr2, Address opr3) {
-        codeBlock.remove(i);
-        codeBlock.add(i, new _3AddressCode(op, opr1, opr2, opr3));
-    }
-
-    public int getCurrentCodeBlockAddress() {
-        return codeBlock.size();
-    }
-
-    public void pintCodeBlock() {
-        System.out.println("Code Block");
-        for (int i = 0; i < codeBlock.size(); i++) {
-            System.out.println(i + " : " + codeBlock.get(i).toString());
-        }
+    public int saveMemory() {
+        _3AddressCode newCode = new _3AddressCode();
+        codeBlock.add(newCode);
+        return codeBlock.indexOf(newCode);
     }
 }
 
